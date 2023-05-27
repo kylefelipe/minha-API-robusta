823c66628ad6fc96fa96fc42590f984052a1ea69
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2023-05-27 11:11:54 -0300

    Adicionando healthcheck

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,17 +6,19 @@ from fastapi.testclient import TestClient
 
 @pytest.fixture
 def client():
-    return TestClient(app)
+    print("tops")
+    test_client = TestClient(app)
+    return test_client
 
 
-def test_quando_verificar_integridade_devo_ter_como_retorno_codigo_de_status_200():
+def test_quando_verificar_integridade_devo_ter_como_retorno_codigo_de_status_200(client):
     response = client.get("/healthcheck")
     assert response.status_code == HTTPStatus.OK
 
-def test_quando_verificar_integridade_formato_de_retorno_deve_ser_json():
+def test_quando_verificar_integridade_formato_de_retorno_deve_ser_json(client):
     response = client.get("/healthcheck")
     assert response.headers["Content-Type"] == "application/json"
     
-def test_quando_verificar_integridade_retorno_deve_ser_igual_a_esperado():
+def test_quando_verificar_integridade_retorno_deve_ser_igual_a_esperado(client):
     response = client.get("/healthcheck")
     assert response.json() == {"status": "ok"}
